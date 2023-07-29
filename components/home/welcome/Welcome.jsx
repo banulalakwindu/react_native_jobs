af441fdef1383a4457892305c1126c2756e0d64b
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-07-29 16:56:34 +0530

    First Commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const Welcome = () => {
+  const router = useRouter()
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Banula</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=''
+            onChange={() => { }}
+            placeholder='What are you looking for?'
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => { }}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage} />
+        </TouchableOpacity>
+      </View>
+      <View>
+
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
